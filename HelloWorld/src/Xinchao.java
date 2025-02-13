ac054419d1a18756e5cc9ead08b7544713bea91a
duquangtrung <duquangtrung2004@gmail.com>
2025-02-13 23:23:20 +0700

    Update Xinchao.java
    Update

--- HelloWorld/src/Xinchao.java
+++ HelloWorld/src/Xinchao.java
@@ -13,6 +13,7 @@ public class Xinchao {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("chào thầy, em lần đầu học java");
+		System.out.println("chào thầy, bây giờ em đi ngủ rồi mai em dậy làm nốt bài hôm nay học, chúc thầy ngủ ngon ạ");
 
 	}
 
