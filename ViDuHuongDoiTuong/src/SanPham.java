375318ec79e9511dc19cd58a10a107213c419736
duquangtrung <duquangtrung2004@gmail.com>
2025-02-19 23:25:46 +0700

    chưa làm xong

--- ViDuHuongDoiTuong/src/SanPham.java
+++ ViDuHuongDoiTuong/src/SanPham.java
@@ -1,17 +1,22 @@
-/**
- * 
- */
+public class CT {
 
-/**
- * 
- */
-public class SanPham {
-
-	/**
-	 * @param args
-	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// tạo ra hai sản phẩm (hardcode)
+		SanPham sp1 = new SanPham();
+		SanPham sp2 = new SanPham(2, "bánh chuối", "đồ an nhanh", "nc.jpg");
+		sp1.setMaSP(1);
+		sp1.setTenSP("BiaSG");
+		sp1.setLoaiSP("Đồ uống có cồn");
+		sp1.setAnhSP("sg.png");
+		
+		//In ra màn hình 2 sản phẩm
+		String thongtinSP1 = sp1.toString();
+		String thongtinSP2 = "Ma san pham 2 la " + sp2.getMaSP();
+		       thongtinSP2 += "Tên SP2 la " + sp2.getTenSP();
+		       
+		System.out.print(thongtinSP1);
+		System.out.print(thongtinSP2);
+		
 
 	}
 
